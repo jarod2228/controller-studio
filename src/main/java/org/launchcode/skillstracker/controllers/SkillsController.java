70fc60136e39c2826a150d07f5738173fb39d229
Jarod Waggener <jarod2228@rocketmail.com>
2021-05-17 19:08:34 -0500

    first commit

---
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -0,0 +1,10 @@
+package org.launchcode.skillstracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+public class SkillsController {
+
+}
